1500d1852091dbf032b2d7b02315a7f7a6f43e18
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-08-05 10:23:50 +0300

    update string

--- lesson_22.py
+++ lesson_22.py
@@ -1,4 +1,5 @@
 def recParentheses(N, paren=None):
+  dstring = ''
   if N == 1:
     res = ['()']
     return res
@@ -9,9 +10,12 @@ def recParentheses(N, paren=None):
         if not paren:
           paren = N
         string = "".join(["(" * paren, ")" * paren])
+        if paren < N-1:
+          dstring = string + "".join(["(" * (N-paren), ")" * (N-paren)]) + ' '
         if paren < N:
+          dstring += '(' + '()'*paren + ')' + '()'*(N-paren-1)
           string += "()" * (N-paren)
-        return [string] + recParentheses(N, paren-1)
+        return [string] + [dstring] + recParentheses(N, paren-1)
 
 def BalancedParentheses(N):
   res = ''
@@ -19,7 +23,7 @@ def BalancedParentheses(N):
   res = recParentheses(N, paren=None)
   for i in range(len(res)):
     if i == 0:
-      result = '"' + result + res[i]+' '
+      result = '"' + result + res[i]
     elif i == len(res)-1:
       result = result + res[i] + '"'
     else:
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 def recParentheses(N, paren=None):
+  dstring = ''
   if N == 1:
     res = ['()']
     return res
@@ -9,9 +10,12 @@ def recParentheses(N, paren=None):
         if not paren:
           paren = N
         string = "".join(["(" * paren, ")" * paren])
+        if paren < N-1:
+          dstring = string + "".join(["(" * (N-paren), ")" * (N-paren)]) + ' '
         if paren < N:
+          dstring += '(' + '()'*paren + ')' + '()'*(N-paren-1)
           string += "()" * (N-paren)
-        return [string] + recParentheses(N, paren-1)
+        return [string] + [dstring] + recParentheses(N, paren-1)
 
 def BalancedParentheses(N):
   res = ''
@@ -19,18 +23,18 @@ def BalancedParentheses(N):
   res = recParentheses(N, paren=None)
   for i in range(len(res)):
     if i == 0:
-      result = '"' + result + res[i]+' '
+      result = '"' + result + res[i]
     elif i == len(res)-1:
       result = result + res[i] + '"'
     else:
       result = result + res[i] +' '
   return result
 
-print(BalancedParentheses(2))
+#print(BalancedParentheses(2))
 #print(BalancedParentheses(3))
 #print(BalancedParentheses(4))
 #print(BalancedParentheses(5))
-#print(BalancedParentheses(6))
+print(BalancedParentheses(6))
 #print(BalancedParentheses(7))
 #print(BalancedParentheses(8))
 #print(BalancedParentheses(9))
